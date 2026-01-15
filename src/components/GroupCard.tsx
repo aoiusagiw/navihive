4e47f16f764978c03a34c2f356afaaab0693c116
aoiusagiw <aoiusagiw@gmail.com>
2026-01-15 20:21:07 +0800

    Update GroupCard.tsx

--- src/components/GroupCard.tsx
+++ src/components/GroupCard.tsx
@@ -303,7 +303,7 @@ const GroupCard: React.FC<GroupCardProps> = ({
         },
         backgroundColor: (theme) =>
           theme.palette.mode === 'dark' ? 'rgba(33, 33, 33, 0.3)' : 'rgba(255, 255, 255, 0.3)',
-        backdropFilter: 'blur(10px)',
+        backdropFilter: 'blur(7px)',
       }}
     >
       <Box
